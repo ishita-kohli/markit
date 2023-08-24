9ece8be10ddcfff5ed84f9e1ca1f001ed591deca
Ishita Kohli <kohli.ishita0302@gmail.com>
2023-08-24 16:07:06 +0530

    something

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -4,6 +4,12 @@ import { AuthContext } from "../modules/auth_provider";
 import { WebsocketContext } from "../modules/websocket_provider";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import DocumentList from "@/components/documentlist";
+
+
+
 
 const Index = () => {
   const [documents, setDocuments] = useState([]);
@@ -48,44 +54,30 @@ const Index = () => {
         <div className="flex justify-center mt-3 p-5">
           <input
             type="text"
-            className="border border-grey p-2 rounded-md focus:outline-none focus:border-blue"
+            className="border border-gray-200 p-2 rounded-md focus:outline-none focus:border-blue-500 w-96"
             placeholder="document name"
             value={documentName}
             onChange={(e) => setDocumentName(e.target.value)}
           />
           <button
-            className="bg-blue-500 border text-white rounded-md px-6 py-2 md:ml-4"
+           className="bg-blue-500 border text-white rounded-md px-6 py-2 md:ml-4"
             onClick={submitHandler}
           >
             Create
           </button>
         </div>
-        <div className="mt-6">
-          <div className="font-bold">Available Documents</div>
-          <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mt-6">
-            {documents.map((document, index) => (
-              <div
-                key={index}
-                className="border border-blue p-4 flex items-center rounded-md w-full"
-              >
-                <div className="w-full">
-                  <div className="text-sm">document</div>
-                  <div className="text-blue font-bold text-lg">
-                    {document.name}
-                  </div>
-                </div>
-                <div className="">
-                  <button
-                    className="px-4 text-white bg-blue rounded-md"
-                    onClick={() => joinDocument(document.id)}
-                  >
-                    join
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <Tabs defaultValue="account" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="owner">Owner</TabsTrigger>
+    <TabsTrigger value="editor">Editor</TabsTrigger>
+    <TabsTrigger value="viewer">Viewer</TabsTrigger>
+  </TabsList>
+  <TabsContent value="owner"><DocumentList role="owner" /></TabsContent>
+  <TabsContent value="editor"><DocumentList role="editor" /></TabsContent>
+  <TabsContent value="viewer"><DocumentList role="viewer" /></TabsContent>
+</Tabs>
+    
       </div>
     </>
   );

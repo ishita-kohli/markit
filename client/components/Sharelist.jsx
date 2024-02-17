3971293c5a05e99f56b07b43ae06d6467662cbe9
Ishita Kohli <kohli.ishita0302@gmail.com>
2024-02-17 18:42:40 +0530

    wip

---
+++ client/components/Sharelist.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import axios from "axios";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Sharelist({ id, sharedWithRoles }) {
+  const [userList, setUserList] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("/users");
+      console.log(data);
+      setUserList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const users = userList.filter((user) =>
+    sharedWithRoles.map((r) => r.user_id).includes(user.id)
+  );
+  return (
+    <div className="flex -space-x-1">
+      {users.map((user) => (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Avatar className="outline-black outline">
+                <AvatarFallback>
+                  {user.username
+                    .split(" ")
+                    .map((s) => s[0].toUpperCase())
+                    .join("")}
+                </AvatarFallback>
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{user.username} - ({sharedWithRoles.find(r => r.user_id === user.id).role})</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ))}
+    </div>
+  );
+}
+
+export default Sharelist;
--- client/pages/edit/[id].jsx
+++ client/pages/edit/[id].jsx
@@ -5,18 +5,13 @@ import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 import ShareDialog from "@/components/ShareDialog";
+import Sharelist from "@/components/Sharelist";
+import { data } from "autoprefixer";
 
 const MDEditor = dynamic(
   () => import("@uiw/react-md-editor").then((mod) => mod.default),
   { ssr: false }
 );
-// const EditorMarkdown = dynamic(
-//   () =>
-//     import("@uiw/react-md-editor").then((mod) => {
-//       return mod.default.Markdown;
-//     }),
-//   { ssr: false }
-// );
 
 function DocumentEditor() {
   const router = useRouter();
@@ -39,7 +34,8 @@ function DocumentEditor() {
         loading: false,
         error: null,
       });
-      setText(data.body)
+      setText(data.body);
+      console.log(`get text: ${data.body}`)
     } catch (err) {
       setDocumentData({
         error: err,
@@ -51,13 +47,42 @@ function DocumentEditor() {
 
   useEffect(() => {
     getDocument();
+    const readInterval = setInterval(getDocument, 1000)
+
+    return () => {
+      clearInterval(readInterval)
+    }
   }, [id]);
+
+  const updateDocument = async (id, bodyText) => {
+    console.log(`saving text: ${bodyText}`)
+    try {
+      await axios.patch(`/document/${id}`, {
+        text: bodyText,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(`saved text: ${bodyText}`)
+  }
+
+  useEffect(() => {
+    if (id) {
+      updateDocument(id, text);
+    }
+  }, [id, text]);
 
   return (
     <div className="flex flex-col w-full">
       <div className="flex justify-between w-full px-8 py-2 mx-auto mb-6 max-w-7xl align-center">
-        <h1 className="text-3xl font-medium uppercase">{documentData.data?.title}</h1>
-        <ShareDialog id={id}/>
+        <h1 className="text-3xl font-medium uppercase">
+          {documentData.data?.title}
+        </h1>
+        <div className="flex items-center gap-x-2">
+        <Sharelist id={id} sharedWithRoles={documentData.data?.roles || []}/>
+        <ShareDialog id={id} />
+        </div>
+       
       </div>
       <div className="container">
         <MDEditor height={650} value={text} onChange={setText} />

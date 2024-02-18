f0f3ff21215b9cdac8f273e9bdd5c9f2a828f69a
Ishita Kohli <kohli.ishita0302@gmail.com>
2024-02-19 01:13:00 +0530

    fix nextjs issues

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
 import AuthContextProvider from "../modules/auth_provider";
 import WebSocketProvider from "../modules/websocket_provider";
-import "./axios.config";
+import "../axios.config";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AuthContextProvider>
         <WebSocketProvider>
-          <div className="flex flex-col md:flex-row h-full min-h-screen font-sans">
+          <div className="flex flex-col h-full min-h-screen font-sans md:flex-row">
             <Component {...pageProps} />
           </div>
         </WebSocketProvider>

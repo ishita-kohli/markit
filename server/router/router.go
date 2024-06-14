954e6a8ceb66e4f9c8552b92e93834e1637a0fe6
Ishita Kohli <100618400+ishita-kohli@users.noreply.github.com>
2024-06-14 19:58:50 +0530

    Update router.go

--- server/router/router.go
+++ server/router/router.go
@@ -13,7 +13,17 @@ var r *gin.Engine
 func InitRouter(userHandler *user.Handler, documentHandler *document.Handler) {
 	r = gin.Default()
 
-	r.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+	    AllowOrigins:     []string{"https://markit-xegd-d2stqv6bx-ishitakohlis-projects.vercel.app", "https://markit-nine.vercel.app"},
+	    AllowMethods:     []string{"PUT", "PATCH", "POST", "OPTIONS", "GET"},
+	    AllowHeaders:     []string{"Origin"},
+	    ExposeHeaders:    []string{"Content-Length"},
+	    AllowCredentials: true,
+	    AllowOriginFunc: func(origin string) bool {
+	      return origin == "https://markit-xegd-d2stqv6bx-ishitakohlis-projects.vercel.app" || origin == "https://markit-nine.vercel.app"
+	    },
+	    MaxAge: 12 * time.Hour,
+	  }))
 
 	r.POST("/signup", userHandler.CreateUser)
 	r.POST("/login", userHandler.Login)
